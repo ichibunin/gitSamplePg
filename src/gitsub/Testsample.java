1858d7be46791c5c0df7c9d8140f3f3c9247486b
CodeCommit-at-037014282963 <ichibunin@gmail.com>
2019-07-14 07:19:17 +0900

    一行追加

--- src/gitsub/Testsample.java
+++ src/gitsub/Testsample.java
@@ -5,7 +5,7 @@ public class Testsample {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("What is your name?");
-
+		System.out.println("My name is Yinyiwen.");
 	}
 
 }
